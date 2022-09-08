e44a9f6a981638f1ae798bbe1962006abf1beec4
Pugpaprika21 <pugpaprika21@gmail.com>
2022-09-08 05:16:26 -0700

    e

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php require_once dirname(__DIR__) . ('../bloodDonationProjectCS60/MyApp/Include/Autoload.php'); ?>
-<?php require_once('../bloodDonationProjectCS60/MyApp/Template/Home/Layout/header.php'); ?>
+<?php require_once('../bloodDonationProjectCS60/MyApp/Template/Index/Layout/header.php'); ?>
 
 <div class="container">
     <a class="btn mt-4 btn-primary" href="../bloodDonationProjectCS60/MyApp/View/Users/home.php?id=1" role="button">Link</a>
 </div>
 
-<?php require_once('../bloodDonationProjectCS60/MyApp/Template/Home/Layout/footer.php'); ?>
+<?php require_once('../bloodDonationProjectCS60/MyApp/Template/Index/Layout/footer.php'); ?>
