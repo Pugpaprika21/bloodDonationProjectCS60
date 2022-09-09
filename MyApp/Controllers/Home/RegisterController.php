ca11b0f095a4b73b0b105fbe220ee583046e6a5e
Pugpaprika21 <pugpaprika21@gmail.com>
2022-09-09 06:17:13 -0700

    e

--- MyApp/Controllers/Home/RegisterController.php
+++ MyApp/Controllers/Home/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace MyApp\Controllers\Home;
 use MyApp\Http\HttpResponse\Response;
+use MyApp\QueryBuilder\AppQuery\Query;
 
 require_once dirname(__DIR__) . ('../../../../bloodDonationProjectCS60/MyApp/Include/Autoload.php');
 
@@ -18,6 +19,9 @@ class RegisterController
         $sql .= " VALUES(:username, :password, :firstname, :lastname, :gender, :dateOfBirth, :bloodType, :weight, :height, :address, :subDistrict, :road, :district, :province, :postCode, :idCard, :email, :phoneNumber)";
 
      
+        $query = (new Query())->insert($sql, [
+            
+        ]);
 
         //Response::render($sql_arr)->jsonString();
     }
